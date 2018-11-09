7f172316d00639e7d4ba63e3df7b5e10a753fbe4
yxzh <cstrikest@gmail.com>
2018-11-10 01:05:53 +0900

    添加特征数据缩放部分，以及其他小改动（提示信息，新增epoch = 1时不输出节）。

--- Multi-GradientDescent.py
+++ Multi-GradientDescent.py
@@ -26,7 +26,6 @@ AT, V, AP, RH, PE = tf.decode_csv(value, record_defaults = record_defaults)
 
 # 加载csv数据
 with tf.Session() as sess:
-	
 	# 线程协调器
 	coord = tf.train.Coordinator()
 	
@@ -66,8 +65,15 @@ print("Data loaded.({:n} data.)".format(m))
 print("\033[1;33m[{:n}s {:n}s/data]\033[0m\n--------------------------------------------------------".format(
 	time.time() - start_time, (time.time() - start_time) / m))
 
-# 标准化
-# TODO: 样本标准化
+# 特征缩放 xn_scale = (xn - average(xn)) / standard(xn)
+sample_scaling = np.ones(shape = (5, m), dtype = np.float64)
+average = np.zeros(shape = (5, 1), dtype = np.float64)
+standard = np.zeros(shape = (5, 1), dtype = np.float64)
+for i in range(1, 5):
+	average[i] = np.mean(sample[i])
+	standard[i] = np.std(sample[i])
+	for j in range(0, m - 1):
+		sample_scaling[i, j] = (sample[i, j] - average[i]) / standard[i]
 
 # 预测函数
 t = tf.Variable(tf.zeros(shape = [1, 5], dtype = tf.float64), dtype = tf.float64)
@@ -79,7 +85,7 @@ y_label = tf.placeholder(tf.float64)
 loss = tf.reduce_sum(tf.square(h - y_label)) / (2 * m)
 
 # 梯度下降
-learning_rate = 1.7e-6
+learning_rate = 1.7e-2
 optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate)
 train = optimizer.minimize(loss)
 
@@ -89,31 +95,31 @@ sess.as_default()
 init = tf.global_variables_initializer()
 sess.run(init)
 iteration = 5000
-step = 100
+step = 25  # 1为不输出节 单步时间默认为每次迭代
 
 # matplotlib 初始化
 ls = [0]
-loss_draw = [sess.run(loss, {x: sample, y_label: label})]
+loss_draw = [sess.run(loss, {x: sample_scaling, y_label: label})]
 loss_temp = 0
 t_0_draw = [0]
 t_1_draw = [0]
 t_2_draw = [0]
 t_3_draw = [0]
 t_4_draw = [0]
-plt.ylim(-3, 3)
+plt.ylim(-15, 7)
 plt.xlim(0, iteration)
 plt.ticklabel_format(style = "plain")
 plt.title("Multi-Regression Test @ " + "{:e}".format(learning_rate) + " Learning Rate")
 
 # 训练
 start_time = time.time()
-print("Start training...")
+print("Training...")
 for epoch in range(1, iteration + 1):
-	sess.run(train, {x: sample, y_label: label})
+	sess.run(train, {x: sample_scaling, y_label: label})
 	
 	# 输出节
-	if epoch % step == 0:
-		loss_temp = sess.run(loss, {x: sample, y_label: label})
+	if step != 1 and epoch % step == 0:
+		loss_temp = sess.run(loss, {x: sample_scaling, y_label: label})
 		print("epoch = {:d}\t\tloss = \033[1;31m{:e}\033[0m \033[1;33m[{:n}s]\033[0m".format(epoch, loss_temp,
 		                                                                                     time.time() - start_time))
 		[[t_0, t_1, t_2, t_3, t_4]] = sess.run(t)
@@ -125,6 +131,8 @@ for epoch in range(1, iteration + 1):
 		ls.append(epoch)
 
 # 输出结果 保存模型
+print("\033[1;33m[{:n}s ({:n}s/step)]\033[0m\n--------------------------------------------------------".format(
+	time.time() - start_time, (time.time() - start_time) / (iteration / step)))
 plt.plot(ls, t_0_draw, "-", color = "red", label = "t_0", linewidth = 1)
 plt.plot(ls, t_1_draw, "-", color = "green", label = "t_1", linewidth = 1)
 plt.plot(ls, t_2_draw, "-", color = "blue", label = "t_2", linewidth = 1)
@@ -132,11 +140,16 @@ plt.plot(ls, t_3_draw, "-", color = "orange", label = "t_3", linewidth = 1)
 plt.plot(ls, t_4_draw, "-", color = "purple", label = "t_4", linewidth = 1)
 plt.legend()
 plt.show()
-print(sess.run(t))
-print("\033[1;33m[{:n}s ({:n}s/step)]\033[0m\n--------------------------------------------------------".format(
-	time.time() - start_time, (time.time() - start_time) / (iteration / step)))
+print("t:", sess.run(t))
 saver = tf.train.Saver()
 saver.save(sess, "model/Multi-Regression.ckpt")
 
 # 测试
-print(sess.run(h, {x: np.array([[1, 14.96, 41.76, 1024.07, 73.17], [1, 33.58, 77.54, 1010.19, 53.49]]).T}))
+
+# 将测试数据缩放
+def scale(n = []):
+	for i in range(1, 5):
+		n[i] = (n[i] - average[i]) / standard[i]
+	return n
+
+print("test:", sess.run(h, {x: np.array([scale([1, 25.18, 62.96, 1020.04, 59.08])]).T}))
