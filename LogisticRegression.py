1f2b2c2deea685f25ea99d77457bb89d7a50d092
yxzh <cstrikest@gmail.com>
2018-11-18 23:42:34 +0900

    逻辑回归部分（有错误，未完成）

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,52 +1,56 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
-# 模型
-W = tf.Variable(2, dtype = tf.float32)
-b = tf.Variable(4, dtype = tf.float32)
-x = tf.placeholder(tf.float32)
-linear_model = W * x + b
-sigmoid_model = 1 / (1 + np.power(np.e, (-1 * linear_model)))  # sigmund
+#  Sigmoid函数
+def sigmoid(z):
+	return 1 / (1 + np.exp(-z))
 
-# 期望 损失函数
-yi = tf.placeholder(np.float32)
-loss_square = -1 * tf.reduce_sum(yi * tf.log(sigmoid_model) + (1 - yi) * tf.log(1 - sigmoid_model)) / 8
-# loss_square = tf.square(loss)
+#  样本与标签
+sample = np.array([[1, 0], [0, 1], [1, 2], [1, 1], [2, 3], [3, 1], [3, 3], [4, 6], [6, 5], [5, 7], [7, 6], [7, 7], [8, 5], [6, 8]])
+label = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+sample = np.insert(sample, 0, np.ones((1, 14)), 1)
 
-# 梯度下降
-learning_rate = 0.000001
-optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-train = optimizer.minimize(loss_square)
+#  预测函数
+t = tf.Variable(tf.ones(shape = [1, 3], dtype = tf.float64), dtype = tf.float64)
+x = tf.placeholder(dtype = np.float64)
+h = 1 / (1 + tf.exp(tf.matmul(t, x)))
+
+#  损失函数 ***此处预测函数有问题，其值永远为NAN。算式应该没有问题，是tf处理的问题。待解决。
+y_label = tf.placeholder(dtype = np.float64)
+loss = tf.reduce_sum(np.multiply(-y_label, tf.log(h)) - np.multiply((1 - y_label), tf.log(1 - x))) * -1 / 14
+
+#  梯度下降
+learning_rate = 1e-3
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate)
+train = optimizer.minimize(loss)
 
 # 初始化
-init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.as_default()
+init = tf.global_variables_initializer()
 sess.run(init)
+iteration = 500
+step = 1  # 1为不输出节 单步时间默认为每次迭代
 
-# matplotlib 初始化
-ls = np.linspace(0, 9, 30)
-plt.title("Logistic Regression Test @ " + str(learning_rate) + " Learning Rate")
-plt.plot([1, 2, 3, 4, 5, 6, 7, 8], [0, 0, 0, 0, 1, 1, 1, 1], "rx", label = "Data")
-plt.plot(ls, sess.run(linear_model, {x: ls}), "-", linewidth = 2.0, label = "Original")
-plt.ylim([-4, 4])
-n_pic = 1
-scale = 300
+# 训练
+start_time = time.time()
+print("Training...")
+print(sample)
+print(label)
+for epoch in range(1, iteration + 1):
+	sess.run(train, {x: sample.T, y_label: label})
+	
+	# 输出节
+	if step != 0 and epoch % step == 0:
+		loss_temp = sess.run(loss, {x: sample.T, y_label: label})
+		print("epoch = {:d}\t\tloss = \033[1;31m{:e}\033[0m \033[1;33m[{:n}s]\033[0m".format(epoch, loss_temp,
+		                                                                                     time.time() - start_time))
 
-# 跑
-for i in range(scale * 5):
-	sess.run(train, {x: [1, 2, 3, 4, 5, 6, 7, 8], yi: [0, 0, 0, 0, 1, 1, 1, 1]})
-	print("i =", i, "fx([1, 2, 3, 4, 5, 6, 7, 8]) =", sess.run(sigmoid_model, {x: [1, 2, 3, 4, 5, 6, 7, 8]}),
-	      "loss_square =",
-	      sess.run(loss_square, {x: [1, 2, 3, 4, 5, 6, 7, 8], yi: [0, 0, 0, 0, 1, 1, 1, 1]}), sess.run((W, b)))
-	if (i + 1) % scale == 0:
-		plt.plot(ls, sess.run(linear_model, {x: ls}), "-", linewidth = 0.6, label = str(n_pic * scale) + " iterations")
-		n_pic += 1
-print("RESULT:", sess.run((W, b)))
-test = 2
-result = sess.run(sigmoid_model, {x: test})
-print(result)
-plt.legend()
-plt.show()
+# 输出结果 保存模型
+print("\033[1;33m[{:n}s ({:n}s/step)]\033[0m\n--------------------------------------------------------".format(
+	time.time() - start_time, (time.time() - start_time) / (iteration / step)))
+saver = tf.train.Saver()
+saver.save(sess, "model/LogisticRegression.ckpt")
